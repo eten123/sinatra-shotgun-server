528a828bb033b28d1d1b25782ecd0a2da1abe5e2
Eten <eten.uket@yale.edu>
2021-07-02 09:18:07 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-  get '/' do 
-    "Welcome to your app!!!! "
-  end
+  
+  get '/' do
+    "Started my server using Shotgun!. I built this!!"
+  end 
 
 end
